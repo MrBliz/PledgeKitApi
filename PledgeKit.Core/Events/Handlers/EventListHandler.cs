470d43d54226f2f8cf5a78f314acd27a0b6151a2
Liam Blizard <work@liamblizard.co.uk>
2018-08-19 18:25:27 +0100

    Updated Event List handler

--- PledgeKit.Core/Events/Handlers/EventListHandler.cs
+++ PledgeKit.Core/Events/Handlers/EventListHandler.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using AzureFromTheTrenches.Commanding.Abstractions;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
+using PledgeKit.Core.Data;
 using PledgeKit.Core.Events.Commands;
 using PledgeKit.Core.Events.Models;
 
@@ -8,9 +12,22 @@ namespace PledgeKit.Core.Events.Handlers
 {
     public class EventListHandler :ICommandHandler<EventListQuery, EventList>
     {
-        public Task<EventList> ExecuteAsync(EventListQuery command, EventList previousResult)
+        private readonly PledgeKitContext _context;
+
+        public async Task<EventList> ExecuteAsync(EventListQuery command, EventList previousResult)
         {
-            throw new NotImplementedException();
+            var query = _context.Events.AsQueryable();
+
+            if(!string.IsNullOrWhiteSpace(command.Filter)){
+                query = query.Where(x=>x.Name.ToLower().Contains(command.Filter.ToLower()));
+            }
+
+            var eventList = new EventList{
+                Events = await  query.Take(command.PageSize)
+                                        .ProjectToType<EventModel>()
+                                        .ToListAsync()  
+                                    };
+            return eventList;
         }
     }
 }
